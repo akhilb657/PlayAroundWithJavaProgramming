2485f3bf2aa48071f73c81af6177393624c1226a
akhil <akhilboyanapalli657@gmail.com>
2024-02-03 20:34:23 -0600

    Playing with numbers

---
+++ com/Java/Fundamentals/IntDataType.java
@@ -0,0 +1,21 @@
+package com.Java.Fundamentals;
+
+public class IntDataType {
+	public static void main(String[] args) {
+		
+		byte a = 4;
+		byte b = 5;
+		int c = a+b; // default reslut is of int type
+		System.out.println(c);
+		
+		int n = 9;
+		System.out.println(n);
+		
+		short d = 11;
+		System.out.println(d);
+		
+		long e = 9999l;
+		System.out.println(e);		
+	}
+
+}
