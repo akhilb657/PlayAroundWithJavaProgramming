e342b8f748b9472b680ef63fef7f5e970f60ae0a
akhil <akhilboyanapalli657@gmail.com>
2024-02-03 21:51:57 -0600

    finding ASCII value of a particular character

---
+++ com/Java/Fundamentals/FindASCIIValueOfACharacter.java
@@ -0,0 +1,20 @@
+package com.Java.Fundamentals;
+
+import java.util.Scanner;
+
+public class FindASCIIValueOfACharacter {
+	public static void main(String[] args) {
+		
+//		Scanner obj = new Scanner(System.in);
+//		
+//		System.out.print("Enter the character: ");
+//		String c = obj.next();
+		
+		char c = 'A';
+		
+		int v = c;
+		
+		System.out.println("ASCII value of " + c + " is " + v);
+	}
+
+}
