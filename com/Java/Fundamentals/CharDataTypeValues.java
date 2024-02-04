124fb569abea0c206c67c200998554e4357bfab6
akhil <akhilboyanapalli657@gmail.com>
2024-02-03 20:26:47 -0600

    working with char DataType

---
+++ com/Java/Fundamentals/CharDataTypeValues.java
@@ -0,0 +1,14 @@
+package com.Java.Fundamentals;
+
+public class CharDataTypeValues {
+	public static void main(String[] args) {
+		//char c = 'aa';
+		
+		char c = 'a';
+		System.out.println(c);
+		
+		char a = 65;
+		System.out.println(a);
+	}
+
+}
