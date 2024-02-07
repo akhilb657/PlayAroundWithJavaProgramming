f733da0ecfc655c4f123157ffdb04acf76e824c4
akhil <akhilboyanapalli657@gmail.com>
2024-02-07 17:50:10 -0600

    Octal, HexaDecimal, Binary

---
+++ com/Java/ConditionalsAndLoops/OctHexBin.java
@@ -0,0 +1,15 @@
+package com.Java.ConditionalsAndLoops;
+
+public class OctHexBin {
+	public static void main(String[] args) {
+		int i = 045;//octal number , corresponding decimal will be given to i
+		System.out.println(i);
+		
+		int j=0x45;//hexaDecimal number , corresponding decimal will be given to i
+		System.out.println(j);
+		
+		int b = 0b10101;//binary number , corresponding decimal will be given to i
+		System.out.println(b);
+	}
+
+}
