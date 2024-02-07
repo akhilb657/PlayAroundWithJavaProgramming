56d398b7da5a97330b977392aad245fcb20c2dff
akhil <akhilboyanapalli657@gmail.com>
2024-02-07 17:33:53 -0600

    Switch Case

---
+++ com/Java/ConditionalsAndLoops/SwitchCase.java
@@ -0,0 +1,23 @@
+package com.Java.ConditionalsAndLoops;
+
+public class SwitchCase {
+	public static void main(String[] args) {
+		int exp = 300;
+		
+		switch(exp){
+			case 100:
+				System.out.println("Case 100 satisfied");
+				break;
+			case 200:
+				System.out.println("Case 200 satisfied");
+				break;
+			case 300:
+				System.out.println("Case 300 satisfied");
+				break;
+			default:
+				System.out.println("None of the above");
+				
+		}
+	}
+
+}
